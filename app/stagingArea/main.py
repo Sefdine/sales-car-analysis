913f4a94731f6c14a7262905eccef93aeb145087
Sefdine Nassuf <74672023+Sefdine@users.noreply.github.com>
2024-01-25 14:03:13 +0100

    change staging area logic

--- app/stagingArea/main.py
+++ app/stagingArea/main.py
@@ -3,13 +3,12 @@
 # Import necessary libraries and load data
 import pandas as pd
 import uuid
-from sqlalchemy import create_engine, MetaData, text, Table, Column, Integer, Float, String, VARCHAR
 
 # Read us cars data from csv
 us_cars_df = pd.read_csv("../data/us_cars.csv")
 
 # Drop unecessary columns
-columns_to_drop = [' Genmodel_ID', 'Wheelbase', 'Annual_Tax', 'Wheelbase', 'Top_speed', 'Engine_power']
+columns_to_drop = [' Genmodel_ID', 'Wheelbase', 'Annual_Tax', 'Wheelbase', 'Top_speed', 'Engine_power', 'Adv_ID']
 us_cars_df.drop(columns=columns_to_drop, inplace=True)
 
 # Rename columns
@@ -25,7 +24,7 @@ us_cars_df.rename(columns=mapper, inplace=True)
 us_cars_df['Stat'] = "used"
 
 # Generate id for us_cars_df
-us_cars_df['ID'] = us_cars_df['Adv_ID'].apply(lambda x: str(uuid.uuid4()))
+us_cars_df['ID'] = us_cars_df['Maker'].apply(lambda x: str(uuid.uuid4()))
 
 # Read the data from the json source
 marketcheck_df = pd.read_json('../data/marketcheck.json')
@@ -38,7 +37,6 @@ marketcheck_mapper = {
     'base_ext_color': 'Color',
     'inventory_type': 'Stat',
     'first_seen_at_source_date': 'Adv_date',
-    'first_seen_at_source': 'Adv_ID',
     'build': 'Build'
 }
 marketcheck_df = marketcheck_df[marketcheck_mapper.keys()]
@@ -80,72 +78,4 @@ marketcheck_df.drop(columns=['Highway_mpg', 'City_mpg'], inplace=True)
 # Concat the two dataframes and create a final dataframe
 final_df = pd.concat([us_cars_df, marketcheck_df], ignore_index=True)
 
-# Function to connect with sql server
-def get_sql_server_connection():
-    server_name = 'LIKE-YOUCODE-DA\SQLEXPRESS'
-    database_name = 'us_car_sales_staging'
-    trusted_connection = 'yes'
-
-    if trusted_connection == 'no':
-        connection_url = f'mssql+pyodbc://{server_name}/{database_name}?driver=SQL+Server'
-    else:
-        username = 'sa'
-        password = 'YourPassword123'
-        connection_url = f"mssql+pyodbc://{username}:{password}@{server_name}/{database_name}?driver=ODBC+Driver+17+for+SQL+Server"
-
-    engine = create_engine(connection_url)
-    return engine
-
-# Create a table in the database
-try:
-    # Retrieve the sql server engine
-    engine = get_sql_server_connection()
-    print(f"Connection to the staging area created successfully.")
-
-    # Inititialize the metadata object
-    meta = MetaData()
-
-    # Create the table
-    us_cars_table = Table(
-        'us_cars_sales_table', meta,
-        Column('id', String(125), primary_key=True),
-        Column('maker', String(125)),
-        Column('model', String(125)),
-        Column('adv_id', String(125)),
-        Column('adv_year', Integer),
-        Column('adv_month', Integer),
-        Column('color', String(125)),
-        Column('reg_year', Integer),
-        Column('bodytype', String(125)),
-        Column('miles', String(125)),
-        Column('engine_size', String(125)),
-        Column('gearbox', String(125)),
-        Column('fuel_type', String(125)),
-        Column('price', Float),
-        Column('height', String(125)),
-        Column('width', String(125)),
-        Column('lenght', String(125)),
-        Column('average_mpg', String(125)),
-        Column('seatings', String(125)),
-        Column('doors', String(125)),
-        Column('stats', String(125)),
-        schema='dbo'
-    )
-
-    try:
-        # Execute the table creation
-        meta.create_all(engine)
-        print('us_car_sales_table created successfully')
-    except Exception as e:
-        print(f'Failed to create the table. {str(e)}')
-except Exception as ex:
-    print("Connection could not be made due to the following error: \n", ex)
-
-# Table name
-table_name = 'us_cars_sales_table'
-
-# Insert the DataFrame into the database
-final_df.to_sql(table_name, con=engine, index=False, if_exists='replace')
-
-# Dispose of the engine 
-engine.dispose()
+final_df.to_csv('../data/final_df.csv', index=False)
