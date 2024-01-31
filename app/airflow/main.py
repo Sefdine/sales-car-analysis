fb3a4a6df073175875ac8458ca1456ab25089288
Sefdine <74672023+Sefdine@users.noreply.github.com>
2024-01-31 12:32:47 +0100

    feat: update dag airflow. data warehousing

--- app/airflow/main.py
+++ app/airflow/main.py
@@ -40,4 +40,141 @@ transformation = BashOperator(
     dag=dag,
 )
 
-staging >> transformation
+# Task: Create tables in data warehouse
+create_tables = BashOperator(
+	task_id = 'create_tables',
+	bash_command='''
+		cd ~/Documents/Projects/Youcode/FilRouge/sales-car-analysis/app/load && \
+		python3 create_tables.py
+	''',
+    dag=dag,
+)
+
+# Task: Insert country dim
+country = BashOperator(
+	task_id = 'country',
+	bash_command='''
+		cd ~/Documents/Projects/Youcode/FilRouge/sales-car-analysis/app/load/tables && \
+		python3 country.py
+	''',
+    dag=dag,
+)
+
+# Task: Insert maker dim
+maker = BashOperator(
+	task_id = 'maker',
+	bash_command='''
+		cd ~/Documents/Projects/Youcode/FilRouge/sales-car-analysis/app/load/tables && \
+		python3 maker.py
+	''',
+    dag=dag,
+)
+
+# Task: Insert color dim
+color = BashOperator(
+	task_id = 'color',
+	bash_command='''
+		cd ~/Documents/Projects/Youcode/FilRouge/sales-car-analysis/app/load/tables && \
+		python3 color.py
+	''',
+    dag=dag,
+)
+
+# Task: Insert gearbox dim
+gearbox = BashOperator(
+	task_id = 'gearbox',
+	bash_command='''
+		cd ~/Documents/Projects/Youcode/FilRouge/sales-car-analysis/app/load/tables && \
+		python3 gearbox.py
+	''',
+    dag=dag,
+)
+
+# Task: Insert car_status dim
+car_status = BashOperator(
+	task_id = 'car_status',
+	bash_command='''
+		cd ~/Documents/Projects/Youcode/FilRouge/sales-car-analysis/app/load/tables && \
+		python3 car_status.py
+	''',
+    dag=dag,
+)
+
+# Task: Insert model dim
+model = BashOperator(
+	task_id = 'model',
+	bash_command='''
+		cd ~/Documents/Projects/Youcode/FilRouge/sales-car-analysis/app/load/tables && \
+		python3 model.py
+	''',
+    dag=dag,
+)
+
+# Task: Insert date dim
+date = BashOperator(
+	task_id = 'date',
+	bash_command='''
+		cd ~/Documents/Projects/Youcode/FilRouge/sales-car-analysis/app/load/tables && \
+		python3 date.py
+	''',
+    dag=dag,
+)
+
+# Task: Insert fuel_dimension
+fuel_dimension = BashOperator(
+	task_id = 'fuel_dimension',
+	bash_command='''
+		cd ~/Documents/Projects/Youcode/FilRouge/sales-car-analysis/app/load/tables && \
+		python3 fuel_dimension.py
+	''',
+    dag=dag,
+)
+
+# Task: Insert fuel_subdimension
+fuel_subdimension = BashOperator(
+	task_id = 'fuel_subdimension',
+	bash_command='''
+		cd ~/Documents/Projects/Youcode/FilRouge/sales-car-analysis/app/load/tables && \
+		python3 fuel_subdimension.py
+	''',
+    dag=dag,
+)
+
+# Task: Insert cars_dimension
+cars_dimension = BashOperator(
+	task_id = 'cars_dimension',
+	bash_command='''
+		cd ~/Documents/Projects/Youcode/FilRouge/sales-car-analysis/app/load/tables && \
+		python3 cars_dimension.py
+	''',
+    dag=dag,
+)
+
+# Task: Insert fact
+fact = BashOperator(
+	task_id = 'fact',
+	bash_command='''
+		cd ~/Documents/Projects/Youcode/FilRouge/sales-car-analysis/app/load/tables && \
+		python3 fact.py
+	''',
+    dag=dag,
+)
+
+staging >> transformation >> create_tables
+
+create_tables >> [
+    country, 
+    maker,
+    color,
+    gearbox, 
+    car_status, 
+    model, 
+    date, 
+    fuel_dimension
+]
+
+fuel_dimension >> fuel_subdimension
+
+[maker, model, color, gearbox, fuel_dimension, car_status] >> cars_dimension
+
+cars_dimension >> fact
